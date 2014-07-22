64b8bfc24f0c2b92f3d4961be843e41b0342234f
Joebh <JoebhJoebh@gmail.com>
2014-07-21 21:31:08 -0400

    initial commit

---
+++ SnowBro/Assets/Sample Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class PlatformerCharacter2D : MonoBehaviour 
+{
+	public float health = 100f;
+	[SerializeField] float jumpForce = 400f;			// Amount of force added when the player jumps.	
+
+	[SerializeField] LayerMask whatIsGround;			// A mask determining what is ground to the character
+	
+	Transform groundCheck;								// A position marking where to check if the player is grounded.
+	float groundedRadius = .2f;							// Radius of the overlap circle to determine if grounded
+	bool grounded = false;								// Whether or not the player is grounded.
+	Transform ceilingCheck;								// A position marking where to check for ceilings
+	float ceilingRadius = .01f;							// Radius of the overlap circle to determine if the player can stand up
+	Animator anim;										// Reference to the player's animator component.
+
+
+    void Awake()
+	{
+		// Setting up references.
+		groundCheck = transform.Find("GroundCheck");
+		ceilingCheck = transform.Find("CeilingCheck");
+		anim = GetComponent<Animator>();
+	}
+
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		//hurt player
+		if(other.gameObject.tag == "fireball"){
+			health -= 10.5f;
+		}
+
+		if (health < 0) {
+			Destroy (gameObject);
+
+			//stop game
+			Time.timeScale = 0;
+			AudioListener.pause = true;
+
+			//show popup
+		}
+
+	}
+
+	void FixedUpdate()
+	{
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
+		anim.SetBool("Ground", grounded);
+
+		// Set the vertical animation
+		anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+	}
+
+
+	public void Move(bool crouch, bool jump)
+	{
+		// If crouching, check to see if the character can stand up
+		if(!crouch && anim.GetBool("Crouch"))
+		{
+			// If the character has a ceiling preventing them from standing up, keep them crouching
+			if( Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround))
+				crouch = true;
+		}
+
+		// Set whether or not the character is crouching in the animator
+		anim.SetBool("Crouch", crouch);
+
+		//only control the player if grounded or airControl is turned on
+		if(grounded)
+		{
+			// The Speed animator parameter is set to the absolute value of the horizontal input.
+			anim.SetFloat("Speed", 1f);
+		}
+
+        // If the player should jump...
+        if (grounded && jump) {
+            // Add a vertical force to the player.
+            anim.SetBool("Ground", false);
+            rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+        }
+	}
+
+}
---
+++ SnowBro/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	public GameObject[] gameObjects;
+	public float spawnMin = 1f;
+	public float spawnMax = 2f;
+
+	void Start(){
+		Spawn ();
+	}
+
+	void Spawn(){		
+		Instantiate (gameObjects [Random.Range (0, gameObjects.Length)], transform.position, Quaternion.identity);
+		Invoke("Spawn", Random.Range(spawnMin, spawnMax));
+	}
+}
